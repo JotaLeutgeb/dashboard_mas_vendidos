df7debe1091927fbfeede26d2609e724962a0d71
Joaquin Leutgeb <joaquin.leut@gmail.com>
2025-09-13 19:55:37 -0300

    Visual Improvements

--- dashboard_oportunidades.py
+++ dashboard_oportunidades.py
@@ -20,11 +20,11 @@ st.markdown("""
     <style>
     /* Elimina el padding superior del contenedor principal */
     .block-container {
-        padding-top: 0rem;
+        padding-top: 0.2rem;
     }
     /* Elimina el padding superior del contenido de la barra lateral */
     [data-testid="stSidebarUserContent"] {
-        padding-top: 0rem;
+        padding-top: 0.2rem;
     }
     </style>
 """, unsafe_allow_html=True)
